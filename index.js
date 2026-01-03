989cd9572ccc68d26aad2642a5c2841efa3a710c
Sourav Bishai <souravbishai58558@gmail.com>
2026-01-03 13:43:23 +0530

    fix fix spl

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 
 app.use(cors());
